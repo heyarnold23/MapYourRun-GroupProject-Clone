c6b4af1063b4836804c12577923b9c0c17c820ae
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-10-11 12:38:34 -0700

    added some map functionality

--- react-app/src/components/CreateRoute/index.js
+++ react-app/src/components/CreateRoute/index.js
@@ -2,13 +2,18 @@ import { NavLink } from 'react-router-dom';
 import {useEffect, useState, useRef} from "react"
 import { useSelector, useDispatch } from 'react-redux';
 import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import "./CreateRoute.css"
+import { getData } from '../../store/geocoding';
 
 const CreateRoute = () => {
+    const dispatch = useDispatch()
     const [coordinates,setCoordinates] = useState([[37.7749,-122.4194],[37.3382,-121.8863],[37.8044,-122.2712],[34.4208,-119.6982],[34.0195,-118.4912],[37.6485,-118.9721],[38.5816,-121.4944],[38.9399,-119.9772]])
     const [errors, setErrors] = useState([]);
     const [startPoint,updateStartPoint] = useState("")
     const [endPoint,updateEndPoint] = useState("")
+    const [forwardUrl,setForwardUrl] = useState("http://api.positionstack.com/v1/forward?access_key=cedc0a3a23e77435267edf0f1c4f9c66&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC")
+    const [reverseUrl,setReverseUrl] = useState("http://api.positionstack.com/v1/forward?access_key=cedc0a3a23e77435267edf0f1c4f9c66&query=40.7638435,-73.9729691")
     let currentCoordinates = coordinates[0]
     mapboxgl.accessToken = 'pk.eyJ1Ijoic3RldmVuYmFybmV0dDEiLCJhIjoiY2t0a2w1bDh1MW13cjJvbnh2Nm4xeHg4ZSJ9.tfF8CCQtdVQSCHxliRtaQQ';
 
@@ -26,17 +31,33 @@ const CreateRoute = () => {
                 center: [lng, lat],
                 zoom: zoom
             });
-            console.log(2)
         });
+
+        useEffect(()=>{
+            navigator.geolocation.getCurrentPosition(function(position) {
+                updateStartPoint(`${position.coords.latitude},${position.coords.longitude}`)
+            });
+        },[])
+
         useEffect(() => {
             if (!map.current) return; // wait for map to initialize
             map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
+                setLng(map.current.getCenter().lng.toFixed(4));
+                setLat(map.current.getCenter().lat.toFixed(4));
+                setZoom(map.current.getZoom().toFixed(2));
             });
-            console.log(3)
-            });
+        });
+        useEffect(()=>{
+            dispatch(getData(forwardUrl))
+
+        },[])
+        let data = useSelector(state=>{
+            if(state.geocoding) return state.geocoding.data
+        })
+        console.log("Geocoding Data: ",data)
+
+
+
 
         return (
             <div id = "create-route-page">
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import geocodingReducer from './geocoding';
 
 const rootReducer = combineReducers({
   session,
+  geocodingReducer
 });
 
 
