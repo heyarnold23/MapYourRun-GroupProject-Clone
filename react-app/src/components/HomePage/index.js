04da785e7689d9d8fe28095738bee6c848ca0ed6
chris <felix.christopher.e@gmail.com>
2021-10-11 14:08:49 -0700

    getting pretty far

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -21,27 +21,56 @@ const HomePage = () => {
           <div className="splash_3_div">
             <div className="app_text_wrapper">
               <div>
-                <div>
-                    <h2>Track Everything</h2>
-                    <p>Log any kind of workout using just
-                      your phone or with your favorite
-                      device like Garmin or Apple Watch.
-                    </p>
-                  </div>
-                  <div>
-                    <h2>Get Social</h2>
-                    <p>Find support and motivation from other runners,
-                      plus create challenges for you and your friends.</p>
-                  </div>
-                  <div>
-                    <h2>Train Smarter</h2>
-                    <p>Analyze your data in the app or on the web, then conquer
-                      that 5K or marathon using our adaptive training plans.</p>
-                  </div>
-                </div>
-                <div>
-                <img src={Splash3} id="splash_3"></img>
-              </div>
+              <h2>Track Everything</h2>
+              <p>Log any kind of workout using just
+                 your phone or with your favorite
+                  device like Garmin or Apple Watch.
+               </p>
+               </div>
+               <div>
+              <h2>Get Social</h2>
+              <p>Find support and motivation from other runners,
+                 plus create challenges for you and your friends.</p>
+                 </div>
+              <h2>Train Smarter</h2>
+              <p>Analyze your data in the app or on the web, then conquer
+                 that 5K or marathon using our adaptive training plans.</p>
+            </div>
+            <img src={Splash3} id="splash_3"></img>
+          </div>
+          <div id="overlap_div">
+            <div className="heading_text">
+              <h2>
+                <hr className="bar"></hr>
+                <span>Find Your Path Anywhere</span>
+                <hr className="bar"></hr>
+              </h2>
+            </div>
+            <p className="subheading">
+              <span>
+              Create and discover new routes wherever <br />
+               you are. Save your favorites for the next <br />
+                time you're ready to run.
+              </span>
+            </p>
+            <div className="cities">
+              <ul className="city_column">
+                <li className="city">
+                  <a href="https://www.youtube.com/watch?v=rTVjnBo96Ug" className="city">
+                    San Francisco
+                  </a>
+                </li>
+                <li className="city">
+                  <a href="https://www.youtube.com/watch?v=s73uAfRSTOw" className="city">
+                    New York
+                  </a>
+                </li>
+                <li className="city">
+                  <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0" className="city">
+                    Los Angeles
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
