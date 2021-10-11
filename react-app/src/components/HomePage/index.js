7462ee9c8c6d1ee3bae46abbc4075a7fed3a3e14
chris <felix.christopher.e@gmail.com>
2021-10-11 16:09:47 -0700

    splash page almost finished

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -6,26 +6,41 @@ import Splash1 from "./public/splash1.png"
 import Splash2 from "./public/splash2.png"
 import Overlay from "./public/splashOverlay.png"
 import Splash3 from "./public/splash3.png"
+import Splash4 from "./public/splash4.png"
+import Splash4Overlay from "./public/splash4Overlay.png"
+import Splash5 from "./public/splash5.png"
 
 const HomePage = () => {
 
     return (
         <div>
           <div className="splash_1_div">
-          <img src={Splash1}></img>
+            <img src={Splash1}></img>
+            <div className="heading_text1">
+              <h2>
+                <hr className="bar1"></hr>
+                <span id="title_1">Own Every Mile</span>
+                <hr className="bar1"></hr>
+              </h2>
+                <p id="p1">The best mobile run tracking experience, <br />
+                  backed by the world's largest digital <br />
+                   health and fitness community.
+                </p>
+                
+            </div>
           </div>
           <div className="splash_2_div">
-          <img src={Splash2}></img>
-          <img src={Overlay} id="overlay_img"></img>
+            <img src={Splash2}></img>
+            <img src={Overlay} id="overlay_img"></img>
           </div>
           <div className="splash_3_div">
             <div className="app_text_wrapper">
               <div>
-              <h2>Track Everything</h2>
-              <p>Log any kind of workout using just
-                 your phone or with your favorite
+                <h2>Track Everything</h2>
+                <p>Log any kind of workout using just
+                  your phone or with your favorite
                   device like Garmin or Apple Watch.
-               </p>
+                </p>
                </div>
                <div>
               <h2>Get Social</h2>
@@ -39,11 +54,11 @@ const HomePage = () => {
             <img src={Splash3} id="splash_3"></img>
           </div>
           <div id="overlap_div">
-            <div className="heading_text">
+            <div className="heading_text2">
               <h2>
-                <hr className="bar"></hr>
+                <hr className="bar2"></hr>
                 <span>Find Your Path Anywhere</span>
-                <hr className="bar"></hr>
+                <hr className="bar2"></hr>
               </h2>
             </div>
             <p className="subheading">
@@ -71,6 +86,14 @@ const HomePage = () => {
                   </a>
                 </li>
               </ul>
+            </div>
+            <div className="splash_4_div">
+              <img src={Splash4} className="map_img"></img>
+              <img src={Splash4Overlay} id="overlay4_img"></img>
+            </div>
+            <div className="splash_5_div">
+            <img src={Splash5}></img>
+
             </div>
           </div>
         </div>
