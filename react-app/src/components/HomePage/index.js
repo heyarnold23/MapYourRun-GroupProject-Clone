4e165638c1d803ba1a403183a94415a8fd111f4c
chris <felix.christopher.e@gmail.com>
2021-10-11 12:54:44 -0700

    well along

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,12 +1,51 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from "react-router-dom"
+import { NavLink } from 'react-router-dom'
+import "./HomePage.css"
+import Splash1 from "./public/splash1.png"
+import Splash2 from "./public/splash2.png"
+import Overlay from "./public/splashOverlay.png"
+import Splash3 from "./public/splash3.png"
 
 const HomePage = () => {
 
     return (
-        <>
-          <h1>Home Page</h1>
-        </>
+        <div>
+          <div className="splash_1_div">
+          <img src={Splash1}></img>
+          </div>
+          <div className="splash_2_div">
+          <img src={Splash2}></img>
+          <img src={Overlay} id="overlay_img"></img>
+          </div>
+          <div className="splash_3_div">
+            <div className="app_text_wrapper">
+              <div>
+                <div>
+                    <h2>Track Everything</h2>
+                    <p>Log any kind of workout using just
+                      your phone or with your favorite
+                      device like Garmin or Apple Watch.
+                    </p>
+                  </div>
+                  <div>
+                    <h2>Get Social</h2>
+                    <p>Find support and motivation from other runners,
+                      plus create challenges for you and your friends.</p>
+                  </div>
+                  <div>
+                    <h2>Train Smarter</h2>
+                    <p>Analyze your data in the app or on the web, then conquer
+                      that 5K or marathon using our adaptive training plans.</p>
+                  </div>
+                </div>
+                <div>
+                <img src={Splash3} id="splash_3"></img>
+              </div>
+            </div>
+          </div>
+        </div>
+        
     )
 }
 
