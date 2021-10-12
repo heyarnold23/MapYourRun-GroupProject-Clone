3782f7718dff9ad80451d0955576bc0a503dd438
Arnold C <acabang23@gmail.com>
2021-10-12 08:01:38 -0700

    finished general layout

--- react-app/src/components/ActivityFeed/index.js
+++ react-app/src/components/ActivityFeed/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import {useDispatch, useSelector} from 'react-redux'
 import {getRunsThunk} from '../../store/runs'
-
+import './ActivityFeed.css'
 
 export default function ActivityFeed() {
   const dispatch = useDispatch()
@@ -15,19 +15,115 @@ export default function ActivityFeed() {
   console.log(runs['1']);
   return (
     <>
-        <div>
+        <div id='middle'>
+          <div id='dropdown'>
+                <button>Explore</button>
+          </div>
             {Object.keys(runs)?.map(id => {
             let run = runs[id]
             return(
+              <>
+              <div id='cardDiv'>
+                <div id='profilePicDiv'>
+                  Picture
+                </div>
+                <div id='mainDetailsDiv'>
+                  <div id='nameDiv'>
+                    <p id='name'>
+                      name went for a run
+                    </p>
+                    <button className='delete'>
+                      X
+                    </button>
+                  </div>
+                  <div id='screenshot'>
+                    screenshot
+                  </div>
+                  <div id='runDetailsDiv'>
+                    <div className='detailDiv'>
+                      <div className='inDetailDiv'>
+                        {run.distance}
+                      </div>
+                      <div className='descriptionDiv'>
+                        Distance(mi)
+                      </div>
+                    </div>
+                    <div className='detailDiv middle'>
+                      <div className='inDetailDiv'>
+                        {run.time}
+                      </div>
+                      <div className='descriptionDiv'>
+                        Avg Pace(min/mi)
+                      </div>
+                    </div>
+                    <div className='detailDiv'>
+                      <div className='inDetailDiv'>
+                        {run.time}
+                      </div>
+                      <div className='descriptionDiv'>
+                        Duration
+                      </div>
+                    </div>
+                  </div>
+                  <div id='lastDiv'>
+                    {/* implement a show button functionality here */}
+                    {/* Maybe make a comments component and have it be a child here on show menu */}
+                    <div id='commentsButton'>
+                      commentsButton
+                    </div>
+                    <div id='createdDate'>
+                      created
+                    </div>
+                  </div>
 
-                <div key={run.id} className='single-run'>
-                    <h1>this is runner id {run.runner_id}</h1>
-                    <h1>this is run id {run.id}</h1>
+
+
+
                 </div>
 
+
+
+
+              </div>
+              </>
             )
             })}
         </div>
+
+        {/* <div id='dropdown'>
+          Explore
+        </div>
+        <div id='cardDiv'>
+          <div id='profilePicDiv'>
+            Placeholder
+          </div>
+          <div id='mainDetailsDiv'>
+            <div id='nameDiv'>
+              name
+            </div>
+            <div id='screenshot'>
+              screenshot
+            </div>
+            <div id='runDetailsDiv'>
+              <div id='distanceDiv'>
+
+              </div>
+
+            </div>
+
+
+
+
+
+          </div>
+
+
+
+
+        </div> */}
+
+
+
     </>
 
   )
